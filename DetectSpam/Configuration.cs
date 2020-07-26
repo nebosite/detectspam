50fdfcb507f7521f1f20a3c1ed4089fd419be1f9
Eric Jorgensen <ericjorg@thejcrew.net>
2020-07-26 15:39:32 -0700

    Refactored to use a config file

---
+++ DetectSpam/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectSpam
+{
+    class MoveListItem
+    {
+        public string HeaderText { get; set; }
+        public string TargetFolder { get; set; }
+    }
+
+    class WordScore
+    {
+        public string RegExp { get; set; }
+        public int Score { get; set; }
+    }
+
+    class Configuration
+    {
+        public string[] SpamFolderPaths { get; set; }
+        public int OKCutoffScore { get; set; }
+        public MoveListItem[] MoveRules { get; set; }
+        public WordScore[] WordScoreRules { get; set; }
+        public string[] WhiteListTextPatterns { get; set; }
+        public string[] WhiteListHtmlPatterns { get; set; }
+    }
+}
